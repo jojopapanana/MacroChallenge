886bfd1fb87b33e84817709fb1f7f595806a8cbc
jojopapanana <jovannamelissa6@gmail.com>
2024-10-01 14:37:36 +0700

    Initial Commit

---
+++ MacroChallenge/MacroChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroChallengeApp.swift
+//  MacroChallenge
+//
+//  Created by Jovanna Melissa on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MacroChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
